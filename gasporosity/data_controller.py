38390469696d017ca267b4c37d56ddd03c4458c1
flirpc <flirpc@livuni.ac.uk>
2025-10-30 11:15:02 +0000

    none check

--- gasporosity/data_controller.py
+++ gasporosity/data_controller.py
@@ -58,6 +58,7 @@ class DataController:
         self.well_count_y = 8
         self.video_writer_name = datetime.datetime.today().strftime("%d%m%y_%H%M")
         self.video_writer = cv2.VideoWriter(f"{self.video_writer_name}.avi",-1,20,(640,480))
+        self.dose_thread = None
         
     def set_well_count(self,x=None,y=None):
         if x is None:
@@ -270,7 +271,7 @@ class DataController:
         self.data = self.video_capture.capture_data()
         frame = cv2.normalize(self.data, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_PLASMA)
-        if self.dose_thread.is_alive():
+        if self.dose_thread is not None and self.dose_thread.is_alive():
             if self.cycle != self.dose_thread.cycle:
                 self.cycle = self.dose_thread.cycle
                 self.video_writer = cv2.VideoWriter(f"{self.video_writer_name}_cycle_{self.cycle}.avi",-1,20,(640,480))
